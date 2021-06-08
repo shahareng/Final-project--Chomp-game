6318ce7adad88b3dc4378e51853f44a0e4f8496c
odeyaMolodic <57504045+odeyaMolodic@users.noreply.github.com>
2021-06-08 11:01:44 +0300

    add codes and graphs

---
+++ Codes/P,N position in 3Xn game.py
@@ -0,0 +1,73 @@
+
+dict = {}
+n = input("Enter n to the size of the board (3*n):")
+# 1 in the first line, 0 in the second and 0 in the third (the first is the lowest)
+dict[(1, 0, 0)] = 'P'
+
+# fill the first line
+for i in range(2,n+1):
+    dict[(i, 0, 0)] = 'N'
+
+
+#fill the second line
+for second in range(1,n+1):
+    for first in range(second,n+1):
+        temp = second-1
+        flag = 1
+        while temp >= 0 & flag:
+            if dict[(first, temp, 0)] == 'P':
+                dict[(first, second, 0)] = 'N'
+                flag = 0
+            temp -= 1
+
+        tempFir = first-1
+        tempSec = second
+        while tempFir > 0 & flag:
+            if second > tempFir:
+                tempSec = tempFir;
+            if dict[(tempFir, tempSec, 0)] == 'P':
+                dict[(first, second, 0)] = 'N'
+                flag = 0
+            tempFir -= 1
+        if flag == 1:
+            dict[(first, second, 0)] = 'P'
+            print(first, second, 0)
+
+# fill the third line
+for third in range(1,n+1):
+    for second in range(third,n+1):
+        for first in range(second, n+1):
+            temp = third - 1
+            flag = 1
+            while temp >= 0 & flag:
+                if dict[(first, second, temp)] == 'P':
+                    dict[(first, second, third)] = 'N'
+                    flag = 0
+                temp -= 1
+
+            tempSec = second -1
+            tempThir = third
+            while tempSec >= 0 & flag:
+                if third > tempSec:
+                    tempThir = tempSec;
+                if dict[(first, tempSec, tempThir)] == 'P':
+                    dict[(first, second, third)] = 'N'
+                    flag = 0
+                tempSec -= 1
+
+            tempFir = first - 1
+            tempSec = second
+            tempThir = third
+            while tempFir > 0 & flag:
+                if second > tempFir:
+                    tempSec = tempFir;
+                if third > tempFir:
+                    tempThir = tempFir;
+                if dict[(tempFir, tempSec, tempThir)] == 'P':
+                    dict[(first, second, third)] = 'N'
+                    flag = 0
+                tempFir -= 1
+            if flag == 1:
+                dict[(first, second, third)] = 'P'
+                print(first, second, third)
+
